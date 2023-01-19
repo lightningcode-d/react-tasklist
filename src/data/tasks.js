1ecbef8f3379c06548ab50d602b8be53e142f26e
DeibyRayo <deiby.developer@gmail.com>
2023-01-19 10:56:40 -0500

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,22 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Hacer el curso de ReactJS",
+    description: "Aprender React modo profesional",
+  },
+  {
+    id: 1,
+    title: "Estudiar Tailwind CSS",
+    description: "Aprender a configurar el entorno",
+  },
+  {
+    id: 2,
+    title: "Suscripción en Platzi",
+    description: "Terminar las escuelas correspondientes",
+  },
+  {
+    id: 3,
+    title: "Mas tareas con la app",
+    description: "Subir la app a un servidor",
+  },
+];
